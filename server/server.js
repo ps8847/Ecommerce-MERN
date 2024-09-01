6237b7d81c74973b7eed51963546c645180a12f4
Prince Sharma <ps789650@gmail.com>
2024-09-01 17:38:08 +0530

    added live link

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ecommerce-mern-woad.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
